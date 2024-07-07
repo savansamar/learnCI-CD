dc0e36eac55c804e47baedefa61dc71cf45214b4
savansamar <savansoni125@gmail.com>
2024-07-08 04:53:52 +0530

    Update UI of Main file

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -1,10 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 
 const Main = () => {
   return (
     <View style={styles.flex}>
-      <Text>Main file</Text>
+      <TouchableOpacity>
+        <Image source={require('../../assets/back.png')} style={styles.imageStyle} />
+      </TouchableOpacity>
+      <Text style={styles.titleStyle}>SET UP YOUR PROFILE</Text>
     </View>
   )
 }
@@ -12,10 +15,20 @@ const Main = () => {
 export default Main
 
 const styles = StyleSheet.create({
-    flex:{
-        flex:1,
-        backgroundColor:"white",
-        justifyContent:"center",
-        alignItems:"center"
-    }
-})
+  flex: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  imageStyle: {
+    height: 30,
+    width: 30,
+    resizeMode: 'contain',
+  },
+  titleStyle:{
+    fontSize:16,
+    letterSpacing:0.4,
+    color:"black"
+  }
+});
